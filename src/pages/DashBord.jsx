caac1cd6e3e490c7d54d4165e4360a3f027cc4a1
Girdhar Kumawat <girdharkumawat3108@gmail.com>
2025-03-30 14:31:29 +0530

    change-1

--- src/pages/DashBord.jsx
+++ src/pages/DashBord.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import Home from "./Home";
 
 
